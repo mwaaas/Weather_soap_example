3efe8c5debcdf82743150ab669c0d54e55ccabad
mwas <francismwangi152@gmail.com>
2014-10-07 12:14:07 +0300

    initial commit

---
+++ weather.py
@@ -0,0 +1,16 @@
+__author__ = 'mwas'
+
+import os
+from suds.client import  Client
+
+# path of weather wsdl file
+weather_wsdl_path = os.path.join(os.path.dirname(__file__), "Weather.asmx.xml")
+
+weather_wsdl_url = "file://" + weather_wsdl_path
+
+weather_client = Client(weather_wsdl_url)
+
+# a simple example for one of the methods
+print weather_client.service.GetWeatherInformation()
+
+
